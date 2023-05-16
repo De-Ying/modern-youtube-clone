ddcff1c1b8a1a2c0a10d47404254cf964ac4b5a6
De-Ying <ducanh25820@gmail.com>
2023-05-16 10:32:18 +0700

    Create Profile Page & Search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,34 @@
 import { Paper, IconButton } from '@mui/material'
 import { Search } from '@mui/icons-material'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const navigate = useNavigate()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`)
+
+      setSearchTerm('')
+    }
+  }
+
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
       sx={{ borderRadius: 20, border: '1px solid #e3e3e3', pl: 2, boxShadow: 'none', mr: { sm: 5 } }}
     >
       <input
         className="search-bar"
         placeholder="Please enter search keyword..."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
       />
       <IconButton
         type="submit"
